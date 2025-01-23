508c53dca0b37dcf308634d7840e067402908c28
tihii-wg <p0d0lian@mail.ru>
2025-01-23 21:47:20 +0200

    add formater

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
-
+const StyledApp = styled.div`
+  background-color: red;
+  padding: 20px;
+`;
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+`;
+const Button = styled.button`
+  font-size: 1.4rem;
+  padding: 1.2rem 1.6rem;
+  font-weight: 500;
+  border: none;
+  border-radius: 7px;
+  background-color: purple;
+  color: white;
+`;
 
 function App() {
   return (
-    <div>
-      Heloo the wild oasis
-    </div>
-  )
+    <StyledApp>
+      <H1>Heloo the wild oasis</H1>
+      <Button onClick={() => alert("ytllo")}>Check in</Button>
+      <Button>Check out</Button>
+    </StyledApp>
+  );
 }
 
-export default App
+export default App;
