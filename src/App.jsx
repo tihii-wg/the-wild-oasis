cf09914480e8f91ae271880d13611eafd4d81e14
tihii-wg <p0d0lian@mail.ru>
2025-01-25 20:05:17 +0200

    creatin Heading Button Input Row

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,20 @@
 import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+import Heading from "./ui/Heading";
+import Row from "./ui/Row";
 
 const StyledApp = styled.div`
-  background-color: red;
   padding: 20px;
-`;
-const H1 = styled.h1`
-  font-size: 30px;
-  font-weight: 600;
-`;
-const Button = styled.button`
-  font-size: 1.4rem;
-  padding: 1.2rem 1.6rem;
-  font-weight: 500;
-  border: none;
-  border-radius: 7px;
-  background-color: purple;
-  color: white;
 `;
 
 function App() {
   return (
-    <StyledApp>
-      <H1>Heloo the wild oasis</H1>
-      <Button onClick={() => alert("ytllo")}>Check in</Button>
-      <Button>Check out</Button>
-    </StyledApp>
+    <>
+      <GlobalStyles />
+      <StyledApp></StyledApp>
+    </>
   );
 }
 
