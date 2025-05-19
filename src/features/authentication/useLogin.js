968adc6fe154b0a17d238c51fa172a2b9488336b
tihii-wg <p0d0lian@mail.ru>
2025-05-19 22:23:04 +0300

    add signup user

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -1,10 +1,7 @@
 import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
 
-import {
-  useMutation,
-  useQueryClient,
-} from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 import { login as loginApi } from "../../services/apiAuth";
 
@@ -15,7 +12,6 @@ export function useLogin() {
   const { mutate: login, isLoading } = useMutation({
     mutationFn: ({ email, password }) => loginApi({ email, password }),
     onSuccess: (user) => {
-      console.log(user);
       queryClient.setQueryData(["user"], user.user);
       navigate("/dashboard", { replace: true });
     },
